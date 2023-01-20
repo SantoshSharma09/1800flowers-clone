c2e005a84c4d3d2e06472ec44669fd61369aebc8
JyotiranjanGhibila <ghibilamohan@gmail.com>
2023-01-20 22:49:09 +0530

    fw20_0878-day-4

---
+++ flower18/src/pages/admin/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getProducts=()=>{
+    return axios.get(` http://localhost:8080/admin`)
+}
+
+export const addProducts=(data)=>{
+    return axios.post(` http://localhost:8080/admin`,{
+        name:data.name,
+        img:data.img,
+        price:data.price
+    })
+}
